c9e15c519d2ae5e32d86c1d6b510a8c202c8a65d
David-Lennuk <153904926+David-Lennuk@users.noreply.github.com>
2024-11-05 14:28:11 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace kolmnurk1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new kolmnurk1());
+            Application.Run(new Form1());
         }
     }
 }
